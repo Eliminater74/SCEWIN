b0e55afb88aa0dc8aa1edc1d054323c37c6aa6e9
Xyueta <101590573+Xyueta@users.noreply.github.com>
2023-09-01 19:11:47 +0200

    Request admin priviliges in `Import.bat`

--- Import.bat
+++ Import.bat
@@ -1,4 +1,14 @@
 @echo off
+:: Request administrator priviliges
+fltmc >nul 2>&1 || (
+    echo info: administrator privileges are required.
+    PowerShell Start -Verb RunAs '%0' 2> nul || (
+        echo info: right-click on the script and select "Run as administrator".
+        pause & exit /b 1
+    )
+    exit /b
+)
+
 pushd %~dp0
 SCEWIN_64.exe /I /S BIOSSettings.txt
 pause
