bb5d57b29553209bd873cadc161aa92632eaad71
AMIT <86924251+amitxv@users.noreply.github.com>
2023-09-09 14:57:23 +0100

    Squashed commit of the following:
    commit 75ea8054e5bb429525d923e792e63afaa9ccdf68
    Author: AMIT <86924251+amitxv@users.noreply.github.com>
    Date:   Sat Sep 9 14:56:32 2023 +0100
    
        Simplify error handling logic
    
    commit 124e528baaa5ceaf7050a641c3426c461cb0d0fd
    Author: Xyueta <101590573+Xyueta@users.noreply.github.com>
    Date:   Fri Sep 1 19:12:55 2023 +0200
    
        Update Import.bat
    
    commit 1386e1e3e2685955574e14d4926c2e4dc4ced39b
    Author: Xyueta <101590573+Xyueta@users.noreply.github.com>
    Date:   Fri Sep 1 19:12:45 2023 +0200
    
        Update Export.bat
    
    commit b0e55afb88aa0dc8aa1edc1d054323c37c6aa6e9
    Author: Xyueta <101590573+Xyueta@users.noreply.github.com>
    Date:   Fri Sep 1 19:11:47 2023 +0200
    
        Request admin priviliges in `Import.bat`
    
    commit 5bc104eb4c5b10102506b678b50864f4a485f3fe
    Author: Xyueta <101590573+Xyueta@users.noreply.github.com>
    Date:   Fri Sep 1 19:11:25 2023 +0200
    
        Request priviliges in `Export.bat`

--- Export.bat
+++ Export.bat
@@ -1,4 +1,13 @@
 @echo off
+:: Request administrator privilieges
+fltmc >nul 2>&1 || (
+    PowerShell Start -Verb RunAs '%0' 2> nul || (
+        echo error: right-click on the script and select "Run as administrator"
+        pause
+    )
+    exit /b 1
+)
+
 pushd %~dp0
 SCEWIN_64.exe /O /S BIOSSettings.txt
 pause
