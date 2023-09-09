75ea8054e5bb429525d923e792e63afaa9ccdf68
AMIT <86924251+amitxv@users.noreply.github.com>
2023-09-09 14:56:32 +0100

    Simplify error handling logic

--- Import.bat
+++ Import.bat
@@ -1,12 +1,11 @@
 @echo off
 :: Request administrator privilieges
 fltmc >nul 2>&1 || (
-    echo info: administrator privileges are required.
     PowerShell Start -Verb RunAs '%0' 2> nul || (
-        echo info: right-click on the script and select "Run as administrator".
-        pause & exit /b 1
+        echo error: right-click on the script and select "Run as administrator"
+        pause
     )
-    exit /b
+    exit /b 1
 )
 
 pushd %~dp0
