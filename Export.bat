77de5252a8acaaba30e954b4cb8c0e818a0febf2
Eliminater74 <Eliminater74@users.noreply.github.com>
2024-01-25 17:16:48 -0500

    Update Export.bat
    Initial Update

--- Export.bat
+++ Export.bat
@@ -8,8 +8,15 @@ fltmc >nul 2>&1 || (
     exit /b 1
 )
 
+:: Change to the directory where the batch file is located
 pushd %~dp0
 
+:: Confirmation for checking the necessary files
+echo This will check for necessary driver files.
+choice /M "Are you sure you want to continue"
+if errorlevel 2 exit /b
+
+:: Loop to check for the existence of each driver file
 for %%a in ("amifldrv64.sys", "amigendrv64.sys") do (
     if not exist "%%~a" (
         echo error: %%~a not found in the current directory
@@ -18,5 +25,19 @@ for %%a in ("amifldrv64.sys", "amigendrv64.sys") do (
     )    
 )
 
+:: Confirmation before running SCEWIN_64.exe
+echo This will run SCEWIN_64.exe with output to nvram.txt and log errors to log-file.txt.
+choice /M "Are you sure you want to proceed"
+if errorlevel 2 exit /b
+
+:: Execute SCEWIN_64.exe with arguments
 SCEWIN_64.exe /O /S nvram.txt 2> log-file.txt
+if errorlevel 1 (
+    echo There was an error running SCEWIN_64.exe. Check log-file.txt for details.
+)
+
+:: Pause to view the SCEWIN_64.exe output
 pause
+
+:: End of the script
+exit /b 0
